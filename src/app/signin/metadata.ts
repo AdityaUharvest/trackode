e210c214f82164186aece2ed4d55f1d2dafd2f62
Aditya Upadhyay <uaditya219@gmail.com>
2025-02-19 21:09:06 +0530

    Refactor metadata structure by centralizing app metadata and adding specific metadata for Sign In and Sign Up pages

---
+++ src/app/signin/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In - Trackode",
+  description: "Log in to access admin and student dashboard , create and manage quizzes, view results and more.",
+};
---
+++ src/app/signup/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up - Trackode",
+  description: "Sing up to access admin and student dashboard , create and manage quizzes, view results and more.",
+};
