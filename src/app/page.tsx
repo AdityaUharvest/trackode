2628a0d92890ee59bca48719fd71d1e548ba86b5
Aditya Upadhyay <uaditya219@gmail.com>
2025-03-06 10:11:48 +0530

    Remove easing property from animation components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
                   delay={150}
                   animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
                   animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-                  easing="easeOutCubic"
+                  
                   threshold={0.2}
                   rootMargin="-50px"
                   onLetterAnimationComplete={handleAnimationComplete}
@@ -73,7 +73,7 @@ export default function Home() {
                   delay={150}
                   animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
                   animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-                  easing="easeOutCubic"
+                 
                   threshold={0.2}
                   rootMargin="-50px"
                   onLetterAnimationComplete={handleAnimationComplete}
