eacdaed29905f94225d148e42a878281ebc86b48
Aditya Upadhyay <uaditya219@gmail.com>
2025-03-28 00:52:04 +0530

    Enhance homepage: add 'Explore Live Quizzes' button and update layout for improved user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function Home() {
                   </svg>
                   Dashboard
                 </Link>
-
+               
               
             </div>
           </div>
@@ -154,14 +154,45 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-col  gap-2 text-center ps-5 pr-5 pb-28">
+        <div className="flex flex-col  gap-2 text-center ps-5 pr-5 pb-12">
           <h3 className={`text-2xl animate-pulse   font-semibold sm:text-3xl ${theme === "dark" ? "text-blue-400" : "text-blue-600"}`}>
             Your Favourite Platform
           </h3>
+          
           <p className={`text-center font-[550] sm:text-xl md:text-3xl ${theme === "dark" ? "text-blue-200" : "text-blue-800"}`}>
             Effortless coding & contests with Trackode
           </p>
         </div>
+        <div className="sm:items-center sm:justify-center mb-7 sm:px-0 sm:space-x-5 sm:flex  ">
+        <Link
+                  href="/dashboard/quiz-list"
+                  title=""
+                  className={`inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold transition-all border-2 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 ${theme === "dark"
+                    ? "text-white border-gray-400 hover:bg-blue-900 focus:bg-blue-900 focus:text-white focus:border-blue-900 bg-blue-500"
+                    : " border-gray-400 hover:bg-blue-800 focus:bg-blue-200 focus:text-black bg-blue-500 text-white focus:border-blue-200"
+                    }`}
+                  role="button"
+                >
+                  <svg
+                    className="w-5 h-5 mr-2"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    stroke="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.18003 13.4261C6.8586 14.3918 5 13.448 5 11.8113V5.43865C5 3.80198 6.8586 2.85821 8.18003 3.82387L12.5403 7.01022C13.6336 7.80916 13.6336 9.44084 12.5403 10.2398L8.18003 13.4261Z"
+                      strokeWidth="2"
+                      strokeMiterlimit="10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  Explore Live Quizzes
+                </Link>
+        </div>
+        
+                
         <div className="lg:max-w-4xl  mb-14 ml-6 mr-6 justify-center flex lg:mx-auto">
           <img
             className="transform rounded-lg  shadow-lg  shadow-blue-800 scale-120"
