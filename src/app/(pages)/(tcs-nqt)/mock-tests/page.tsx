b2f34112a344891f0d69982944c7366790f6a166
Aditya Upadhyay <uaditya219@gmail.com>
2025-03-31 17:31:27 +0530

    Refactor NewMockTestPage: remove unnecessary div wrapper around MockTestForm

--- src/app/(pages)/(tcs-nqt)/mock-tests/page.tsx
+++ src/app/(pages)/(tcs-nqt)/mock-tests/page.tsx
@@ -2,8 +2,8 @@ import MockTestForm from "@/components/MockTestForm";
 
 export default function NewMockTestPage() {
   return (
-    <div className="container mx-auto p-4">
+   
       <MockTestForm />
-    </div>
+    
   );
 }
