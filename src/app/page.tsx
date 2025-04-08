4c91e3c49ff4dded3398288571d2fc6b3cd43337
Aditya Upadhyay <131547655+iamadityaupadhyay@users.noreply.github.com>
2025-04-09 03:30:17 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import HomePage from './HomePage'
 import StructuredData from '@/components/StructuredData'
 
 export const metadata: Metadata = {
-  title: 'Trackode - Master Coding Skills with Interactive Quizzes & Contests',
-  description: 'Trackode helps developers track their coding journey through interactive quizzes, AI-generated challenges, and coding contests. Improve your programming skills effectively.',
+  title: 'Trackode - Free Mocks Test and Interactive',
+  description: 'Trackode helps developers track their knowledge through interactive quizzes, AI-generated challenges.Improve your programming skills effectively.',
 }
 export default function Home() {
   return (
